e151a2611a91fa127cd3640a6b2d27b598476c9b
poap <poapoap@gmail.com>
2017-05-29 20:05:47 +0900

    Added data collecting part and input part

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from instance import TemperatureInstance
 from predictor import TemperaturePredictor
+import random
 import keras
 import numpy as np
 import sys
@@ -12,56 +13,71 @@ import matplotlib.pylab as plt
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-DATA_PATH = r'C:\Users\coffee\Desktop\8\cs570\tempredict\data\processed_data_inner.csv'
+DATA_DIR = os.path.join(os.curdir, "data")
 
 
 def main():
     print("python:{}, keras:{}, tensorflow: {}".format(sys.version, keras.__version__, tf.__version__))
-    instance = TemperatureInstance(DATA_PATH)
+    instance = TemperatureInstance(os.path.join(DATA_DIR, "processed_data_inner.csv"))
+    files = os.listdir(DATA_DIR)
+    files.remove('processed_data_inner.csv')
+    files.remove('processed_data_outer.csv')
+    files.remove('README.txt')
+
     # predictor = TemperaturePredictor(instance)
     # predictor.predict()
 
-    xntrn = round(len(instance.x_mat) * 0.9)
+    x_raw = [instance.x_mat[:]]
+    y_raw = [instance.y_mat[:]]
+    x_label = instance.x_label
+    y_label = instance.y_label
 
-    x_train = instance.x_mat[0:xntrn]
-    x_test = instance.x_mat[xntrn:]
-    y_train = instance.y_mat[0:xntrn]
-    y_test = instance.y_mat[xntrn:]
+    for f in files:
+        instance = TemperatureInstance(os.path.join(DATA_DIR, f))
+        x_raw.append(instance.x_mat[:])
+        y_raw.append(instance.y_mat[:])
 
-    in_neurons = len(instance.x_label)
-    # in_neurons = 2
-    out_neurons = len(instance.y_label)
-    # out_neurons = 2
+    in_neurons = len(x_label)
+    out_neurons = len(y_label)
     hidden_neurons = 100
+    data_interval = 20
 
     model = Sequential()
     model.add(LSTM(hidden_neurons, return_sequences=False,
                    input_shape=(None, in_neurons)))
-    model.add(Dense(out_neurons, input_dim=hidden_neurons))
-    model.add(Activation("linear"))
+    model.add(Dense(out_neurons, input_dim=hidden_neurons, activation="linear"))
+    # model.add(Activation("linear"))
     model.compile(loss="mean_squared_error", optimizer="rmsprop")
-
     model.summary()
-    # print(np.shape(x_train))
-    # print(np.shape(y_train))
-    xx_train = np.array([x_train[i:i + 10] for i in range(len(x_train) - 10)])
-    yy_train = np.array([y for y in y_train[10:]])
-    # print(np.shape(xx_train))
-    # print(np.shape(yy_train))
-    xx_test = np.array([x_test[i:i + 10] for i in range(len(x_test) - 10)])
-    yy_test = np.array([y for y in y_test[10:]])
-    model.fit(xx_train, yy_train, batch_size=5, epochs=100, validation_split=0.05)
+
+    x_data = []
+    y_data = []
+    test_index = random.randint(0, len(x_raw)-1)
+    x_t_data = x_raw.pop(test_index)
+    y_t_data = y_raw.pop(test_index)
+    for idx in range(len(x_raw)):
+        x = x_raw[idx]
+        y = y_raw[idx]
+        x_data += [x[i:i + data_interval] for i in range(len(x) - data_interval)]
+        y_data += [y_ for y_ in y[data_interval:]]
+    x_t_data = [x_t_data[i:i + data_interval] for i in range(len(x_t_data) - data_interval)]
+    y_t_data = [y for y in y_t_data[data_interval:]]
+
+    xx_train = np.array(x_data)
+    yy_train = np.array(y_data)
+    xx_test = np.array(x_t_data)
+    yy_test = np.array(y_t_data)
+    model.fit(xx_train, yy_train, batch_size=20, epochs=100, validation_split=0.05, verbose=2)
 
     predicted = model.predict(xx_test)
-    rmse = np.sqrt(((predicted - yy_test) ** 2).mean(axis=0))
-    print(rmse)
 
     plt.rcParams["figure.figsize"] = (17, 9)
     plt.plot(predicted[:100][:, 0], "--")
     plt.plot(predicted[:100][:, 1], "--")
     plt.plot(yy_test[:100][:, 0], ":")
     plt.plot(yy_test[:100][:, 1], ":")
-    plt.legend(["Prediction 0", "Prediction 1", "Test 0", "Test 1"])
+    legend = ["Pred_" + l for l in y_label] + ["Real_" + l for l in y_label]
+    plt.legend(legend)
     plt.show()
 
 if __name__ == '__main__':
